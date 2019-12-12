1285990f007e0b3c6ca3aebb33454587e056c826
Andrey Ovcharov <andrey.ovcharov@influencerdb.com>
2019-12-12 12:05:08 +0100

    .

--- include/fpc1020.h
+++ include/fpc1020.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <esp_system.h>
+
 typedef struct
 {
     uint8_t command_done : 1;
@@ -8,26 +10,76 @@ typedef struct
     uint8_t finger_down : 1;
 } fpc1020_interrupt_t;
 
+typedef enum
+{
+    SLEEP,
+    DEEP_SLEEP,
+    IDLE
+} fpc1020_sleep_mode_t;
+
+typedef struct
+{
+    uint16_t wait_for_finger : 7;
+    uint16_t sleep : 7;
+} fpc1020_detect_t;
+
+// init the defice
 esp_err_t fpc1020_init();
 
-esp_err_t fpc1020_get_hwid(uint16_t *hwid);
-
-esp_err_t fpc1020_get_finger_present_status(uint16_t *status);
-
-esp_err_t fpc1020_capture_image();
-
-esp_err_t fpc1020_read_image();
-
+// Read interrupt status
+// 18, 1C
 esp_err_t fpc1020_read_interrupt(fpc1020_interrupt_t *interrupt, uint8_t clear);
 
+// 20
 esp_err_t fpc1020_finger_present_query();
 
+// 24
 esp_err_t fpc1020_wait_for_finger();
 
-esp_err_t fpc1020_sleep(uint8_t mode);
+// 28, 2C, 34
+esp_err_t fpc1020_sleep(fpc1020_sleep_mode_t mode);
 
+// 38
+esp_err_t fpc1020_get_error(uint8_t* error);
+
+// 3C
+esp_err_t fpc1020_get_clkbist();
+
+// 54
+esp_err_t fpc1020_get_image_capture_size();
+
+// 78
+esp_err_t fpc1020_get_test_pattern();
+
+// 7C
+esp_err_t fpc1020_get_clkbist_result();
+
+// 8C
+esp_err_t fpc1020_get_finger_drive_conf();
+
+// 94
+esp_err_t fpc1020_get_ost_trim();
+
+// A0
+esp_err_t fpc1020_get_adc_shift_gain();
+
+// C0
+esp_err_t fpc1020_capture_image();
+
+// C4
+esp_err_t fpc1020_read_image();
+
+// D4
+esp_err_t fpc1020_get_finger_present_status(uint16_t *status);
+
+// D8
+esp_err_t fpc1020_get_finger_det_thrs(uint8_t *threshold);
+
+// DC
+esp_err_t fpc1020_get_finger_det_cntr(fpc1020_detect_t *query);
+
+// F8
 esp_err_t fpc1020_soft_reset();
 
-esp_err_t fpc1020_clkbist();
-
-esp_err_t fpc1020_irq_status(uint8_t *status);
+// FC
+esp_err_t fpc1020_get_hwid(uint16_t *hwid);
